fc490e5338da10f269bd1c9447799e341c212175
Jorge Arias <ja549655@gmail.com>
2024-12-29 19:19:17 -0400

    Implementacion de la comparacion v1

--- Agentes.cpp
+++ Agentes.cpp
@@ -29,7 +29,7 @@ struct Persona
 
 ifstream Archivo;
 //EXISTEN VALORES FIJOSQUE NO SON ALTERABLES?
-
+//estas funciones solo aseguran que los valores proporcionados estan en los intervalos correspondientes
 bool ValidarTamanio(float A)
 {
   if ((1<=A+Rango_Error && A-Rango_Error<=20)||(1<=A-Rango_Error && A+Rango_Error<=20))
@@ -68,8 +68,7 @@ bool ValidarDistanciaNarizLabio(float A){
 }
 
 
-void CantidadPersonas(int& numero)
-{
+void CantidadPersonas(int& numero){
    Archivo.open("dataBase.in", ios::in); // se abre el archivo en modo lectura
    
     if (Archivo.fail())
@@ -84,6 +83,7 @@ void CantidadPersonas(int& numero)
    numero = stoi(linea);
 Archivo.close();
 }
+
 Persona* CargarElementos(){
      Archivo.open("dataBase.in", ios::in); // se abre el archivo en modo lectura
 
@@ -101,8 +101,7 @@ Persona* CargarElementos(){
     for (int i = 0; i < cantidadPersonas; i++)
     {
         Persona p;
-        p.Id = i + 1; // Asignar un ID único
-
+        p.Id = i; // Asignar un ID único
         getline(Archivo, p.Nombre); // Leer nombre
         getline(Archivo, p.Especie); // Leer especie
 
@@ -112,7 +111,16 @@ Persona* CargarElementos(){
 
         // Leer capacidad mágica y convertir a booleano
         getline(Archivo, linea);
-        p.Magia = (linea == "true");
+        if (linea=="Si"||linea=="si"||linea=="SI")
+        {
+          p.Magia = true;
+        }
+        else
+        {
+          p.Magia = false;
+        }
+        
+       
 
         // Leer medidas de la cara
         getline(Archivo, linea);
@@ -132,69 +140,8 @@ Persona* CargarElementos(){
     }
     Persona *Arreglo = personas;
     delete[] personas; // Liberar memoria del arreglo temporal 
- 
     Archivo.close(); // Cerrar el archivo
     return Arreglo;
-}
-bool Validar_Cara(Cara A,Cara B){
-  
-  bool Rasgos_Coincidentes = false;
-  int Cont =0;
-  if (A.Distancia_Frente_Nariz==B.Distancia_Frente_Nariz)
-  {
-    Cont++;
-  }
-  if (A.Distancia_Nariz_Labio==B.Distancia_Nariz_Labio)
-  {
-    Cont++;
-  }
-  if (A.Distancia_Ojos==B.Distancia_Ojos)
-  {
-    Cont++;
-  }
-  bool Ojos =false;
-  if (A.Profundidad_Ojos<B.Profundidad_Ojos)
-  {
-    
-    Ojos=true;
-  }
-  
-    if (Cont>=2&&Ojos)
-    {Rasgos_Coincidentes= true;
-        return Rasgos_Coincidentes;
-    }
-return Rasgos_Coincidentes;
-
-}
-
-bool Sospechoso(Persona A){//Validamossi es un posible sospechoso
- if (A.Especie==Especies[1])//Si es un kripsan esta fera de sospechas
- {
-  return false;
- }
-  if (A.Magia==true)//Si tiene magia estaffuera de sospechas
- {
-  return false;
- }
-return true;
- 
-}
-bool TamanioSospechoso(Persona A, Persona B){//Validamossi existe un cambioen su tamanio que pueda ser motivo de sospecha
-
-if (A.Altura<B.Altura+Rango_Error||A.Altura<B.Altura-Rango_Error)
-{
-  return true;
-}
-
-}
-bool EsCambiaFormas(Persona A, Persona B)//Validamos si hay cambios en las formas de las personas  
-{
-  if (Sospechoso(A)&&Sospechoso(B))//Validamos si ambos son sospechosos
-  {
-    
-    
-  }
-  
 }
 
 
@@ -203,7 +150,8 @@ void ImprimirLista(Persona *Personas,int N){
   for (int i = 0; i < N; i++)
   {
     cout<<"--------------"<<endl;
-    cout<<"Persona "<<i+1<<endl;
+  
+    cout<<"Persona "<<Personas[i].Id<<endl;
     cout<<"Nombre: "<<Personas[i].Nombre<<endl;
     cout<<"Especie: "<<Personas[i].Especie<<endl;
     cout<<"Altura: "<<Personas[i].Altura<<endl;
@@ -213,12 +161,103 @@ void ImprimirLista(Persona *Personas,int N){
   
 
 }
+
+void LLenarId(Persona *Personas,int N){
+
+  for (int i = 0; i < N; i++)
+  {
+      Personas[i].Id=i;
+  }
+  
+
+}
+bool AlturaSospechosa(Persona Base,Persona Clon){
+     if (ValidarTamanio(Base.Altura)&&ValidarTamanio(Clon.Altura))
+     {
+       if (Base.Altura<Clon.Altura||Base.Altura>Clon.Altura)
+       {
+        return true;
+       }
+     }
+     return false;
+     
+}
+bool ProfundidadOjosSospechosa(Persona Base,Persona Clon){
+    if (ValidarProfundidadOjos(Base.Mi_Rostro.Profundidad_Ojos)&&ValidarProfundidadOjos(Clon.Mi_Rostro.Profundidad_Ojos))
+     {
+       if (Base.Mi_Rostro.Profundidad_Ojos<Clon.Mi_Rostro.Profundidad_Ojos||Base.Mi_Rostro.Profundidad_Ojos >Clon.Mi_Rostro.Profundidad_Ojos)
+       {
+        return true;
+       }
+     }
+     return false;
+}
+bool CompartenRasgosSospechoso(Persona Base,Persona Clon){
+  if (Base.Mi_Rostro.Distancia_Frente_Nariz==Clon.Mi_Rostro.Distancia_Frente_Nariz)
+  {
+    return true;
+  }
+  if (Base.Mi_Rostro.Distancia_Nariz_Labio==Clon.Mi_Rostro.Distancia_Nariz_Labio)
+  {
+    return true;
+  }
+  
+  if (Base.Mi_Rostro.Distancia_Ojos==Clon.Mi_Rostro.Distancia_Ojos)
+  {
+    return true;
+  }
+  return false;
+
+}
+bool GrupoSospechoso(Persona Base,Persona A,Persona B){
+int coincidencia=0;
+//VALIDAMOS FRENTE NARIZ
+  if (Base.Mi_Rostro.Distancia_Frente_Nariz==A.Mi_Rostro.Distancia_Frente_Nariz&&Base.Mi_Rostro.Distancia_Frente_Nariz==B.Mi_Rostro.Distancia_Frente_Nariz)
+  {
+    coincidencia++;
+  }
+  //VALIDAMOS NARIZ LABIO
+  if (Base.Mi_Rostro.Distancia_Nariz_Labio == A.Mi_Rostro.Distancia_Nariz_Labio && Base.Mi_Rostro.Distancia_Nariz_Labio==B.Mi_Rostro.Distancia_Nariz_Labio)
+  {
+    coincidencia++;
+  }
+  //VALIDAMOS DISTANCIA OJOS
+   if (Base.Mi_Rostro.Distancia_Ojos == A.Mi_Rostro.Distancia_Ojos&& Base.Mi_Rostro.Distancia_Ojos==B.Mi_Rostro.Distancia_Ojos)
+  {
+    coincidencia++;
+  }
+  
+  if (coincidencia>2)
+  {
+    return true;
+  }
+  return false;
+  
+}
+
+
+bool EsCambiaFormas(Persona Base,Persona Clon){
+if (CompartenRasgosSospechoso(Base,Clon))
+{
+  if (ProfundidadOjosSospechosa(Base,Clon))
+  {
+    if (AlturaSospechosa(Base,Clon))
+    {
+      return true;
+    }
+    
+  }
+}
+return false;
+}
 int main(){
 int PersonasCanti;
 Persona *Personas;
 CantidadPersonas(PersonasCanti);//ALMACENA LA CANTIDAD PERSONAS 
 Personas = CargarElementos();
+LLenarId(Personas,PersonasCanti);
 ImprimirLista(Personas,PersonasCanti);
+cout<<EsCambiaFormas(Personas[0],Personas[2]);
 return 0;
 }
  
